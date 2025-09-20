e14e7502e30ff22b0d6aa52405547a179d96fe98
mohamed-y <24-mohamedyehia>
2025-09-20 23:34:09 +0300

    refactor main.py to streamline imports and include base router

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI     # import FastAPI class from fastapi package
+from routes import base     # this is the file we created
+from dotenv import load_dotenv    # this package is used to load environment variables from .env file
+load_dotenv(".env")
 
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {
-        "message": "Welcome to the API"
-    }
+app.include_router(base.base_router)
