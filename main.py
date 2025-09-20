95f2dfa014534809418d791028cfe3e5fba8c672
mohamed-y <24-mohamedyehia>
2025-09-21 01:37:28 +0300

    refactor main.py to correct router imports and streamline code

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI     # import FastAPI class from fastapi package
-from routes import base     # this is the file we created
-from dotenv import load_dotenv    # this package is used to load environment variables from .env file
-load_dotenv(".env")
+from fastapi import FastAPI   
+from src.routes import base_router, data_router
 
 app = FastAPI()
 
-app.include_router(base.base_router)
+app.include_router(base_router)
+app.include_router(data_router)
